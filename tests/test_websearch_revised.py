271715846c2b9fac97a1f2695b9ad5ce1355a14d
billytan430 <st3204@columbia.edu>
2023-12-11 12:12:16 -0500

    Replaced old test_web_search with revised version

--- tests/test_websearch_revised.py
+++ tests/test_websearch_revised.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
 import sys
@@ -9,7 +9,9 @@ import os
 sys.path.append("/Users/shuaitaotan/Tan_Shuaitao_homework/tan_shuaitao_homework/wgertrainer")
 
 from websearch import search_exercise_on_youtube
+import pytest
 
+@pytest.mark.parametrize("query", ["push ups", "yoga"])
 def test_search_exercise_on_youtube(query):
     """
     Test the search_exercise_on_youtube function.
@@ -24,6 +26,12 @@ if __name__ == "__main__":
     queries = ["push ups", "yoga"]
     for query in queries:
         test_search_exercise_on_youtube(query)
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
